9d6a4895775437d5df1c5df38a6c5489f3f17bf5
BozeBro <62128128+BozeBro@users.noreply.github.com>
2020-07-11 11:07:51 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,13 +12,16 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 MAGENTA = (255, 0, 255)
 
-wait_time = 100
+wait_time = 20
+fps = float("inf")
 clock = pygame.time.Clock()
 
-surface = Surface(color=BLUE)
+game_values = {"rows": 10, "columns": 10, "blocksize": 30}.values()
+
+surface = Surface(*game_values, color=BLUE, caption="Snake Game")
 screen = surface.make_screen()
-snake = Snake(screen, surface.surface_data, color=MAGENTA)
-apple = Apple(snake.snake, surface.surface_data)
+snake = Snake(screen, game_values, color=MAGENTA)
+apple = Apple(snake.snake, game_values, color=RED)
 pygame.display.flip()
 running = True
 
@@ -26,22 +29,21 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    clock.tick(float("inf"))
+    clock.tick(fps)
+    pygame.time.wait(wait_time)
     snake.get_user_move()
     pygame.time.wait(wait_time)
-    # allow user the time to make a move
+    # allow user time to make a move
     tail = snake.snake.popleft()
     if not apple.exists:
         snake.snake.appendleft(tail)
-        apple.spawn_apple(
-            screen, surface.rows, surface.columns, surface.blocksize, apple.color
-        )
+        apple.make_apple(screen)
+    pygame.time.wait(wait_time)
     snake.move_snake(screen, tail, surface.color)
     snake.move_snake(screen, snake.snake[-1], snake.color)
-    running = (False, True)[
-        apple.update(snake.snake[-1], tail) and snake.in_itself()
-        ]
+    pygame.time.wait(wait_time)
+    running = (False, True)[apple.update(snake.snake[-1], tail) and snake.in_itself()]
+    pygame.time.wait(wait_time)
     # update() updates available cords for apple spawns and checks for wall collision
     # in_itself() checks snake collision with its body
 print("FINISHED")
-
